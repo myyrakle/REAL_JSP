48f17727c3a06185a3591fe750bda3c4fe585eac
myyrakle <sssang97@naver.com>
2020-08-14 07:49:58 +0900

    add init

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const VIEW_DIRECTORY_NAME = "views";
-
-const root = require("app-root-path");
-const fs = require("fs");
-const path = require("path");
+const { PAGE_PATH, PUBLIC_PATH, ROOT_PATH } = require("./init");
 
 const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static("./public"));
+app.use(express.static(PUBLIC_PATH));
 
-app.set("views", `./${VIEW_DIRECTORY_NAME}`);
+app.set("views", PAGE_PATH);
 app.set("view engine", "ejs");
 
-const filenames = fs.readdirSync(
-    path.join(`${root.path}`, VIEW_DIRECTORY_NAME)
-);
+const fs = require("fs");
+const filenames = fs.readdirSync(PAGE_PATH);
 
 filenames.forEach((filename) => {
+    console.log(filename);
     app.get(filename, (req, res) => {
         return res.render(filename, { request: req.query });
     });
     app.post(filename, (req, res) => {
+        return res.render(filename, { request: req.body });
+    });
+    app.delete(filename, (req, res) => {
+        return res.render(filename, { request: req.query });
+    });
+    app.put(filename, (req, res) => {
         return res.render(filename, { request: req.body });
     });
 });
@@ -29,7 +31,7 @@ filenames.forEach((filename) => {
 app.start = async () => {
     const port = app.port || 8888;
     console.log("!!!!!!");
-    console.log(root.path);
+    console.log(ROOT_PATH);
     app.listen(port, () => {
         console.log(`>>>> App started ${port}`);
     });
---
+++ init.js
@@ -0,0 +1,19 @@
+const root = require("app-root-path");
+const fs = require("fs");
+const path = require("path");
+
+const PAGE_PATH = path.join(root.path, "page");
+const PUBLIC_PATH = path.join(root.path, "public");
+try {
+    if (fs.statSync(PAGE_PATH).isDirectory() == false) throw null;
+} catch {
+    fs.mkdirSync(PAGE_PATH);
+}
+
+try {
+    if (fs.statSync(PUBLIC_PATH).isDirectory() == false) throw null;
+} catch {
+    fs.mkdirSync(PUBLIC_PATH);
+}
+
+module.exports = { PAGE_PATH, PUBLIC_PATH, ROOT_PATH: root.path };
