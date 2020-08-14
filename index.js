e0b49ce5725e29771d098007a9fafd56083aec69
myyrakle <sssang97@naver.com>
2020-08-14 10:15:39 +0900

    getfile impl

--- index.js
+++ index.js
@@ -4,32 +4,43 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(PUBLIC_PATH));
+app.use("/public", express.static(PUBLIC_PATH));
 
 app.set("views", PAGE_PATH);
 app.set("view engine", "ejs");
 
 const fs = require("fs");
-const filenames = fs.readdirSync(PAGE_PATH);
+const path = require("path");
 
-filenames.forEach((filename) => {
-    console.log(filename);
-    app.get(filename, (req, res) => {
-        return res.render(filename, { request: req.query });
+function getFiles(dir) {
+    const subdirs = fs.readdirSync(dir);
+    const files = subdirs.map((subdir) => {
+        const res = path.resolve(dir, subdir);
+        return fs.statSync(res).isDirectory() ? getFiles(res) : res;
     });
-    app.post(filename, (req, res) => {
-        return res.render(filename, { request: req.body });
+
+    return files.reduce((a, f) => a.concat(f), []);
+}
+
+getFiles(PAGE_PATH).forEach((filename) => {
+    const URL = ""; //PATH 설정할 것
+
+    app.get(URL, (req, res) => {
+        res.render(filename, { method: "get", request: req.query });
     });
-    app.delete(filename, (req, res) => {
-        return res.render(filename, { request: req.query });
+    app.post(URL, (req, res) => {
+        res.render(filename, { method: "post", request: req.body });
     });
-    app.put(filename, (req, res) => {
-        return res.render(filename, { request: req.body });
+    app.delete(URL, (req, res) => {
+        res.render(filename, { method: "delete", request: req.query });
+    });
+    app.put(URL, (req, res) => {
+        res.render(filename, { method: "put", request: req.body });
     });
 });
 
 app.start = async () => {
-    const port = app.port || 8888;
+    const port = app.port || 12345;
     console.log("!!!!!!");
     console.log(ROOT_PATH);
     app.listen(port, () => {
