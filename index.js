daca63989cad10eedf0c57f0c75bae639fd9470d
myyrakle <sssang97@naver.com>
2020-08-16 19:29:53 +0900

    구조 개선

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const { PAGE_PATH, PUBLIC_PATH, ROOT_PATH } = require("./init");
 
 const express = require("express");
 const app = express();
+
+app.PAGE_PATH = PAGE_PATH;
+app.PUBLIC_PATH = PUBLIC_PATH;
+app.ROOT_PATH = ROOT_PATH;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/public", express.static(PUBLIC_PATH));
@@ -22,27 +27,27 @@ function getFiles(dir) {
   return files.reduce((a, f) => a.concat(f), []);
 }
 
-getFiles(PAGE_PATH).forEach((filename) => {
-  const PATH = path.relative(ROOT_PATH, filename).replace(/\\/g, `/`);
-
-  app.get(PATH, (req, res) => {
-    res.render(filename, { method: "get", request: req.query });
-  });
-  app.post(PATH, (req, res) => {
-    res.render(filename, { method: "post", request: req.body });
-  });
-  app.delete(PATH, (req, res) => {
-    res.render(filename, { method: "delete", request: req.query });
-  });
-  app.put(PATH, (req, res) => {
-    res.render(filename, { method: "put", request: req.body });
-  });
-});
-
 app.start = async () => {
+  getFiles(app.PAGE_PATH).forEach((filename) => {
+    const PATH = path.relative(app.ROOT_PATH, filename).replace(/\\/g, `/`);
+
+    app.get(PATH, (req, res) => {
+      res.render(filename, { method: "get", request: req.query });
+    });
+    app.post(PATH, (req, res) => {
+      res.render(filename, { method: "post", request: req.body });
+    });
+    app.delete(PATH, (req, res) => {
+      res.render(filename, { method: "delete", request: req.query });
+    });
+    app.put(PATH, (req, res) => {
+      res.render(filename, { method: "put", request: req.body });
+    });
+  });
+
   const port = app.port || 12345;
   console.log("!!!!!!");
-  console.log(ROOT_PATH);
+  console.log(app.ROOT_PATH);
   app.listen(port, () => {
     console.log(`>>>> App started ${port}`);
   });
