aa6ee463fa42437ffebed678f9e0d9dec390fbb6
myyrakle <sssang97@naver.com>
2020-08-16 19:26:19 +0900

    경로 설정 구현

--- index.js
+++ index.js
@@ -13,39 +13,39 @@ const fs = require("fs");
 const path = require("path");
 
 function getFiles(dir) {
-    const subdirs = fs.readdirSync(dir);
-    const files = subdirs.map((subdir) => {
-        const res = path.resolve(dir, subdir);
-        return fs.statSync(res).isDirectory() ? getFiles(res) : res;
-    });
+  const subdirs = fs.readdirSync(dir);
+  const files = subdirs.map((subdir) => {
+    const res = path.resolve(dir, subdir);
+    return fs.statSync(res).isDirectory() ? getFiles(res) : res;
+  });
 
-    return files.reduce((a, f) => a.concat(f), []);
+  return files.reduce((a, f) => a.concat(f), []);
 }
 
 getFiles(PAGE_PATH).forEach((filename) => {
-    const URL = ""; //PATH 설정할 것
+  const PATH = path.relative(ROOT_PATH, filename).replace(/\\/g, `/`);
 
-    app.get(URL, (req, res) => {
-        res.render(filename, { method: "get", request: req.query });
-    });
-    app.post(URL, (req, res) => {
-        res.render(filename, { method: "post", request: req.body });
-    });
-    app.delete(URL, (req, res) => {
-        res.render(filename, { method: "delete", request: req.query });
-    });
-    app.put(URL, (req, res) => {
-        res.render(filename, { method: "put", request: req.body });
-    });
+  app.get(PATH, (req, res) => {
+    res.render(filename, { method: "get", request: req.query });
+  });
+  app.post(PATH, (req, res) => {
+    res.render(filename, { method: "post", request: req.body });
+  });
+  app.delete(PATH, (req, res) => {
+    res.render(filename, { method: "delete", request: req.query });
+  });
+  app.put(PATH, (req, res) => {
+    res.render(filename, { method: "put", request: req.body });
+  });
 });
 
 app.start = async () => {
-    const port = app.port || 12345;
-    console.log("!!!!!!");
-    console.log(ROOT_PATH);
-    app.listen(port, () => {
-        console.log(`>>>> App started ${port}`);
-    });
+  const port = app.port || 12345;
+  console.log("!!!!!!");
+  console.log(ROOT_PATH);
+  app.listen(port, () => {
+    console.log(`>>>> App started ${port}`);
+  });
 };
 
 module.exports = app;
